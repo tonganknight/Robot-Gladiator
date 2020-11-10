c2cf12aaea6f90e50fdd1b453719daa32a1fab51
Malachi <malachialusa@gmail.com>
2020-11-10 15:16:10 -0700

    added basic functions to robotrounds

--- assets/js/game.js
+++ assets/js/game.js
@@ -10,7 +10,18 @@ var enemyName = "Roborto";
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var enemyNames = ["Roborto", "Amy Android","Robo Trumble"];
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+/*for(var i = 0; i < enemyName.length; i++) {
+  console.log(enemyNames[i]);
+  console.log(i);
+  console.log(enemyNames[i] + " is at " + i + " index");
+}*/
+
+
+var fight = function(enemyName) {
   // Alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
 
@@ -45,10 +56,22 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
   }
   // if player choses to skip
 } else if (promptFight === "skip" || promptFight === "SKIP") {
-  window.alert(playerName + " has chosen to skip the fight!");
-} else {
-  window.alert("You need to choose a valid option. Try again!");
-}
+    // confirm player wants to skip
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+    // if yes (true), leave fight
+    if (confirmSkip) {
+      window.alert(playerName + " has decided to skip this fight. Goodbye!");
+      // subtract money from playerMoney for skipping
+      playerMoney = playerMoney - 2;
+    }
+    // if no (false), ask question again by running fight() again
+    else {
+      fight();
+    }
+  }
 
 }
-fight();
+for(var i=0; i< enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
